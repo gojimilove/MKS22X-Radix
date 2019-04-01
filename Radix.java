392d3ec47e8a62ef8af08d80d86adbbbad85a077
Jessica Kim <jkim00@stuy.edu>
2019-04-01 11:46:14 -0400

    sorting works for 2 digit numbers but its all hard coding right now, added method for easy printing arrays

--- Radix.java
+++ Radix.java
@@ -1,5 +1,36 @@
 public class Radix {
   public static void radixsort(int[]data){
-    int[][] buckets = new int[20][data.length];
+    int[] buckets = new int[20];//make 2d array butfor now only one value in each bucket
+    //int k = 2;//length of longest number in data, 2 for now
+    for (int i = 0; i < data.length; i++) {
+      buckets[data[i]%10 + 10] = data[i];
+    }
+    printArray(buckets);
+    int[] buckets2 = new int[20];
+    for (int i = 0; i < buckets.length; i++) {
+      if (buckets[i] != 0) {
+        buckets2[(buckets[i]/10)%10 + 10] = buckets[i];
+      }
+    }
+    printArray(buckets2);
+    //starting with ones place
+      //if number is negative buckets[9-(first digit of num)] gets num added to it
+      //else buckets[(first digit of num) + 10] gets num added to import junit.framework.TestCase;
+    //move on to next place value, repeat
+  }
+
+  public static void printArray(int[] data) {
+    String s = "[";
+    for (int i = 0; i < data.length; i++) {
+      s+=data[i];
+      if (i < data.length-1) s+=", ";
+    }
+    s+="]";
+    System.out.println(s);
+  }
+
+  public static void main(String[]args) {
+    int[] tester = new int[]{45, 13, 57, 24, 32, 86, 90};
+    radixsort(tester);
   }
 }

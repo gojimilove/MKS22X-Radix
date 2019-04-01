14d323867cfd2cf4df5dd0087a1bff5f0ad982e8
Jessica Kim <jkim00@stuy.edu>
2019-03-31 22:28:57 -0400

    removed indexOf(E) and contains(E) and added <E> to Node to fix compiletime errors

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,28 +1,28 @@
 class Node<E> {
 	private E data;
-	private Node next,prev;
+	private Node<E> next,prev;
 
 	public Node(E x) {
 		data = x;
 		next = null;
 		prev = null;
 	}
-	public Node next() {
+	public Node<E> next() {
 		return next;
 	}
-	public Node prev() {
+	public Node<E> prev() {
 		return prev;
 	}
-	public void setNext(Node other) {
+	public void setNext(Node<E> other) {
 		next = other;
 	}
-	public void setPrev(Node other) {
+	public void setPrev(Node<E> other) {
 		prev = other;
 	}
-	public Integer getData() {
+	public E getData() {
 		return data;
 	}
-	public Integer setData(E i) {
+	public E setData(E i) {
 		data = i;
 		return data;
 	}
@@ -37,25 +37,42 @@ class Node<E> {
 
 public class MyLinkedList<E> {
 	private int length;
-	private Node start,end;
+	private Node<E> start,end;
 
 	public MyLinkedList() {
 		//empty list
 		length = 0;
 	}
 
+	public int size() {
+		return length;
+	}
+
 	public void clear() {
 		length = 0;
+	}
+
+	private Node<E> getNode(int index) {
+		if (index < 0 || index >= length) {
+			throw new IndexOutOfBoundsException();
+		}
+		Node<E> current = start;
+		//go through linked list until either you reach the end or you reach the index, then return that node
+		while(current.getData() != null && index > 0) {
+			current = current.next();
+			index--;
+		}
+		return current;
 	}
 
 	public boolean add(E value) {
 		//System.out.println("Length before: "+length);
 		if (length == 0) { //list is empty, make a new node start and end both point to
-			start = new Node(value);
+			start = new Node<E>(value);
 			end = start;
 		}
 		else { //start and end both point to something
-			Node n = new Node(value);
+			Node<E> n = new Node<E>(value);
 			//current last value now points to new node, vice versa
 			end.setNext(n);
 			n.setPrev(end);
@@ -73,15 +90,15 @@ public class MyLinkedList<E> {
 		else if (index == length) add(value);
 		//node at index before this one now points to this and vice versa
 		else if (index == 0) {
-			Node n = new Node(value);
+			Node<E> n = new Node<E>(value);
 			start.setPrev(n);
 			n.setNext(start);
 			start = n;
 		}
 		else {
-			Node before = getNode(index - 1);
-			Node after = getNode(index);
-			Node n = new Node(value);
+			Node<E> before = getNode(index - 1);
+			Node<E> after = getNode(index);
+			Node<E> n = new Node<E>(value);
 			before.setNext(n);
 			n.setPrev(before);
 			//node at index now points to node previously at index
@@ -92,62 +109,18 @@ public class MyLinkedList<E> {
 		length++;
 	}
 
-	public int size() {
-		return length;
-	}
-
-	private Node getNode(int index) {
-		if (index < 0 || index >= length) {
-			throw new IndexOutOfBoundsException();
-		}
-		Node current = start;
-		//go through linked list until either you reach the end or you reach the index, then return that node
-		while(current.getData() != null && index > 0) {
-			current = current.next();
-			index--;
-		}
-		return current;
-	}
-
-	public boolean contains(E value) {
-		//go through list, if it's not in it return false
-		Node current = start;
-		while(current != null) {
-			if (current.getData().equals(value)) {
-				return true; //if value matches then return true
-			}
-			current = current.next();
-		}
-		return false;
-	}
-
-	public int indexOf(E value) {
-		//return -1 if value does not exist
-		if (!contains(value)) {
-			return -1;
-		}
-		int i = 0;
-		Node current = start;
-		//loop through until you find the value, then stop and return the index
-		while(current != null && !current.getData().equals(value)) {
-			current = current.next();
-			i++;
-		}
-		return i;
-	}
-
 	public E removeFront() {
 		if (length <= 0) {
 			throw new IndexOutOfBoundsException("the linked list is empty");
 		}
-		Node n = getNode(0);
+		Node<E> n = getNode(0);
 		E x = n.getData();
 		start = getNode(1);
 		getNode(1).setPrev(null);
 		length--;
 		return x;
 	}
-	
+
 	public void extend(MyLinkedList<E> other) {
 		this.length = this.size() + other.size();
 		this.end.setNext(other.start);
@@ -160,7 +133,7 @@ public class MyLinkedList<E> {
 
 	public String toString() {
 		String result = "[";
-		Node current = start;
+		Node<E> current = start;
 		while (current != null) {
 			result += current.getData();
 			if (current.next() != null) {
